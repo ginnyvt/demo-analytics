ebaa137771b00e16a358dc1b14295ec7bab7266a
Ginny <59504133+ginnyvt@users.noreply.github.com>
2022-12-28 15:05:47 +0200

    feature/uploadfile (#1)
    * chore: add csv parser and multer
    
    * feat: hanle csv file uploaded
    
    * feat: create endpoint to upload file and process uploaded data
    
    * fix: sales_day and sales_time format
    
    * feat: make database connection using docker
    
    * feat: insert data to db
    
    * chore: add gitignore and install packages

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const uploadRoutes = require("./routes/upload.route");
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -9,6 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
 	return res.send("Received a GET HTTP method");
 });
+
+app.use("/api", uploadRoutes);
 
 const PORT = process.env.port || 8000;
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
