c0eba43cd1e398a17a06d7d0f85ab9d70a34c6d2
ginnyvt <ginny@powerkernel.com>
2023-01-02 14:49:13 +0200

    chore: setup k8s to deploy db and server

--- server/src/config/db.config.js
+++ server/src/config/db.config.js
@@ -1,8 +1,8 @@
 const dbConfigs = {
-	HOST: "localhost",
-	USER: "root",
-	PASSWORD: "example",
-	DB: "mydb",
+	HOST: process.env.DATABASE_HOST,
+	USER: process.env.DATABASE_USER,
+	PASSWORD: process.env.DATABASE_PASS,
+	DB: process.env.DATABASE_NAME,
 };
 
 module.exports = dbConfigs;
